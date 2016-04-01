f696df0375c0bf4f4f837bab275f2a29859f1bc6
himanshu <himanshg12@gmail.com>
2016-04-01 20:00:28 +0530

    raw designs

---
+++ app/src/main/java/com/example/android/vaccinationplan/HospitalDetail.java
@@ -0,0 +1,13 @@
+package com.example.android.vaccinationplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_detail);
+    }
+}
---
+++ app/src/main/java/com/example/android/vaccinationplan/VaccinesCompleted.java
@@ -0,0 +1,13 @@
+package com.example.android.vaccinationplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VaccinesCompleted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccines_completed);
+    }
+}
