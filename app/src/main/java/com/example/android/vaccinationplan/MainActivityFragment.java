a237ff0514af4c1ff1c48ae286ece1e2aa380270
himanshu <himanshg12@gmail.com>
2016-04-02 20:52:54 +0530

    home view loaded

--- app/src/main/java/com/example/android/vaccinationplan/MainActivityFragment.java
+++ app/src/main/java/com/example/android/vaccinationplan/MainActivityFragment.java
@@ -9,19 +9,42 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> mVaccineAdapter;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] vaccinationList = {"asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv","asdasldalj dihfayfv",};
+
+        List<String> vaccineList = new ArrayList<String>(Arrays.asList(vaccinationList));
+
+        //create an ArrayAdaptar from the String Array
+        mVaccineAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                R.layout.main_list_view,
+                R.id.code,
+                vaccineList);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(mVaccineAdapter);
+
+        return rootView;
     }
 
     @Override
