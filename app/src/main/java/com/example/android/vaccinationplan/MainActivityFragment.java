7a62b614dacede8f22b67a545c999a69619596f7
himanshu <himanshg12@gmail.com>
2016-04-10 21:57:27 +0530

    responsivenss to designs

--- app/src/main/java/com/example/android/vaccinationplan/MainActivityFragment.java
+++ app/src/main/java/com/example/android/vaccinationplan/MainActivityFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
@@ -61,6 +62,26 @@ public class MainActivityFragment extends Fragment {
                 vaccineList);*/
 
         ListView listView = (ListView) rootView.findViewById(R.id.listView);
+
+        /* Screen Height adjustment */
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < mVaccineAdapter.getCount(); i++) {
+            view = mVaccineAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, RadioGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (mVaccineAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+
+
+
         listView.setAdapter(mVaccineAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
