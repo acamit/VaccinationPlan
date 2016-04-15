3e6f43a8f5ec4453b43f551eab7e461bb1fad368
Amit <acamit84@gmail.com>
2016-04-15 16:09:10 +0530

    Child Details Date Picker added

--- app/src/main/java/com/example/android/vaccinationplan/ChildDetailActivityFragment.java
+++ app/src/main/java/com/example/android/vaccinationplan/ChildDetailActivityFragment.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.Toast;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -21,11 +21,8 @@ import java.util.Calendar;
  * A placeholder fragment containing a simple view.
  */
 public class ChildDetailActivityFragment extends Fragment{
-    private DatePicker datePicker;
     private Calendar calendar;
-   /* private TextView dateView;
-    private int year, month, day;
-   */ private View rootView;
+    private View rootView;
     private Button changeDate;
     private SharedPreferences pref;
     private static final String DATE_FORMAT = "yyyy-MM-dd";
@@ -62,16 +59,7 @@ public class ChildDetailActivityFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         mContext = getContext();
-       // dateView = (TextView) rootView.findViewById(R.id.date_view);
         calendar = Calendar.getInstance();
-        /*year = calendar.get(Calendar.YEAR);
-
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        *///showDate(year, month+1, day);
-
-
-
 
         num_of_children =1;
         pref = PreferenceManager.getDefaultSharedPreferences(mContext);
@@ -91,45 +79,18 @@ public class ChildDetailActivityFragment extends Fragment{
 
 
 
-    //@SuppressWarnings("deprecation")
+  /*  //@SuppressWarnings("deprecation")
     public void setDate(View view) {
 
         Toast.makeText(getActivity().getApplicationContext(), "ca", Toast.LENGTH_SHORT)
                 .show();
-    }
+    }*/
 
     public void updateDateButtonText() {
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
         String dateForButton = dateFormat.format(calendar.getTime());
         changeDate.setText(dateForButton);
     }
-/*
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(getActivity(), myDateListener, year, month, day);
-        }
-        return null;
-    }
-
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-            // TODO Auto-generated method stub
-            // arg1 = year
-            // arg2 = month
-            // arg3 = day
-            showDate(arg1, arg2 + 1, arg3);
-        }
-    };
-
-    private void showDate(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-    }*/
-
-
     class DatePickerFragment extends DialogFragment implements
             DatePickerDialog.OnDateSetListener {
 
